e0eb4b450475fabe04f979426354038accedd577
Emilia Holmqvist <emiliaholmqvist@live.se>
2025-04-21 11:46:47 +0200

    Delete Downloads directory

--- Downloads/Yum Yum Gimme Sum EH/src/storage/data.js
+++
@@ -1,4 +0,0 @@
-import { fetchMenu } from "../api/api.js"
-
-export let menuNew = await fetchMenu()
-
--- Downloads/Yum Yum Gimme Sum EH/src/utils/calculateTotal.js
+++
@@ -1,8 +0,0 @@
-
-
-export const calculateTotal = (array) => {
-    const total = array.reduce((acc, item) => {
-        return acc + (item.price * item.quantity * 1.25);
-    }, 0);
-    return Math.round(total * 100) / 100;
-}
